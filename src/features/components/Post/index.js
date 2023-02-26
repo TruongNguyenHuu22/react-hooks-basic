4f795d2b0db50686212ec956397fbd1c9fc10970
HenryNguyen <nhtruong2@gmail.com>
2023-02-26 13:20:25 +0700

    useEffect search

--- src/features/components/Post/index.js
+++ src/features/components/Post/index.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
-import Pagination from "../Pagination";
+import Pagination from "./Pagination";
 import PostList from "./PostList";
 import queryString from "query-string";
+import PostFilterForm from "./PostFilterForm";
 
 const PostFeature = () => {
     const [postList, setPostList] = useState([]);
@@ -13,6 +14,7 @@ const PostFeature = () => {
     const [filters, setFilters] = useState({
         _limit: 10,
         _page: 1,
+        title_like: "",
     });
 
     const handlePageChange = (newPage) => {
@@ -39,9 +41,14 @@ const PostFeature = () => {
         fetchPostList();
     }, [filters]);
 
+    const handleFiltersChange = (newFilters) => {
+        setFilters({ ...filters, _page: 1, title_like: newFilters.searchTerm });
+    };
+
     return (
         <>
             <h3>UseEffect - Post List</h3>
+            <PostFilterForm onSubmit={handleFiltersChange} />
             <PostList posts={postList} />
             <Pagination
                 pagination={pagination}
