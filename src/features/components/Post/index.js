82e292dfa22662665011ae1ccc567985ebbe5173
HenryNguyen <nhtruong2@gmail.com>
2023-02-26 11:45:18 +0700

    useEffect postList pagination

--- src/features/components/Post/index.js
+++ src/features/components/Post/index.js
@@ -1,29 +1,52 @@
 import React, { useEffect, useState } from "react";
+import Pagination from "../Pagination";
 import PostList from "./PostList";
+import queryString from "query-string";
 
 const PostFeature = () => {
     const [postList, setPostList] = useState([]);
+    const [pagination, setPagination] = useState({
+        _page: 1,
+        _limit: 10,
+        _totalRows: 1,
+    });
+    const [filters, setFilters] = useState({
+        _limit: 10,
+        _page: 1,
+    });
+
+    const handlePageChange = (newPage) => {
+        setFilters({
+            ...filters,
+            _page: newPage,
+        });
+    };
 
     useEffect(() => {
         async function fetchPostList() {
+            const paramString = queryString.stringify(filters);
             try {
-                const requestUrl =
-                    "http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1";
+                const requestUrl = `http://js-post-api.herokuapp.com/api/posts?${paramString}`;
                 const response = await fetch(requestUrl);
                 const responseJSON = await response.json();
-                const { data } = responseJSON;
+                const { data, pagination } = responseJSON;
                 setPostList(data);
+                setPagination(pagination);
             } catch (error) {
                 console.log("Failed to Post list", error.message);
             }
         }
         fetchPostList();
-    }, []);
+    }, [filters]);
 
     return (
         <>
             <h3>UseEffect - Post List</h3>
             <PostList posts={postList} />
+            <Pagination
+                pagination={pagination}
+                onPageChange={handlePageChange}
+            />
         </>
     );
 };
